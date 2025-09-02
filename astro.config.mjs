ec1e7f2e4347996529b0442a15950948d4b98d56
Dulcemaria465 <a2233336125@alumnos.uat.edu.mx>
2025-09-02 16:36:46 -0600

    Config Astro para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dulcecv.tech', // tu dominio personalizado
-  output: 'static',             // 👈 obliga a Astro a generar solo HTML/CSS/JS
+  site: 'https://dulcecv.tech', // tu dominio personalizado (opcional, útil para rutas absolutas y sitemap)
+  output: 'static'              // obliga a Astro a generar solo HTML/CSS/JS
 });
